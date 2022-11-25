a897202751ab0352048ca3928bf62fb228eb42b1
rasmane226 <yameogorasmane99@gmail.com>
2022-11-25 10:34:03 +0300

    test

--- Models/Kayit.cs
+++ Models/Kayit.cs
@@ -14,5 +14,7 @@ namespace PersonelYonetim.Models
         public int id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
+
+        public string test { get; set; }
     }
 }
