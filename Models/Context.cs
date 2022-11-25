b95dd0b8773aedcbc728cd89128d0e2d1d719138
rasmane226 <yameogorasmane99@gmail.com>
2022-11-25 10:23:53 +0300

    deneme iptal edildi

--- Models/Context.cs
+++ Models/Context.cs
@@ -10,12 +10,13 @@ namespace PersonelYonetim.Models
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server=DESKTOP-V1MJ2V1;database=personelYonetim;integrated security=true;");
+            optionsBuilder.UseSqlServer("server=BIM-TEKNIK-SERV;database=personelYonetim;integrated security=true;");
         }
         public DbSet<Personel> personels { get; set; }
         public DbSet<Unvan> Unvans { get; set; }
       
         public DbSet<Admin> admins{ get; set; }
+        public DbSet<Kayit> kayits{ get; set; }
 
     }
 }
---
+++ Models/Kayit.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System;
+
+using System.Linq;
+using System.Threading.Tasks;
+namespace PersonelYonetim.Models
+{
+    public class Kayit
+    {
+        [Key]
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
